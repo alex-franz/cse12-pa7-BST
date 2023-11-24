f3a27cd207976e9940ceb943d851ca2af098aba7
alex-franz <alfranz@ucsd.edu>
2023-11-23 22:44:12 -0800

    File System Update

--- pa7-starter/src/FileData.java
+++ pa7-starter/src/FileData.java
@@ -4,13 +4,21 @@ public class FileData {
     public String dir;
     public String lastModifiedDate;
 
-    // TODO
     public FileData(String name, String directory, String modifiedDate) {
-        // Stub TEST
+        this.name = name;
+        this.dir = directory;
+        this.lastModifiedDate = modifiedDate;
     }
-
-    // TODO
+    /*
+     * A method that returns the string representation of 
+     * FileData by displaying the file information. 
+     * It should strictly follow the format of 
+     * {Name: file_name, Directory: dir_name, Modified Date: date}.
+     */
+    @Override
     public String toString() {
-        return null;
+        // STUB
+        String str = "{Name: "+this.name+", Directory: "+this.dir+", Modified Date: "+this.lastModifiedDate+"}";
+        return str;
     }
 }
--- pa7-starter/src/FileSystem.java
+++ pa7-starter/src/FileSystem.java
@@ -4,6 +4,7 @@ import java.util.LinkedList;
 import java.util.Comparator;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.foreign.Arena;
 import java.util.Scanner;
 
 
@@ -12,21 +13,24 @@ public class FileSystem {
     BST<String, FileData> nameTree;
     BST<String, ArrayList<FileData>> dateTree;
     
-    // TODO
     public FileSystem() {
-
+        this.nameTree = new BST<String,FileData>();
+        this.dateTree = new BST<String,ArrayList<FileData>>();
     }
 
 
-    // TODO
     public FileSystem(String inputFile) {
-    	// Add your code here
+        this.nameTree = new BST<String,FileData>();
+        this.dateTree = new BST<String,ArrayList<FileData>>();
         try {
             File f = new File(inputFile);
             Scanner sc = new Scanner(f);
             while (sc.hasNextLine()) {
                 String[] data = sc.nextLine().split(", ");
-                // Add your code here
+                nameTree.put(data[0], new FileData(data[0], data[1], data[2]));
+                ArrayList<FileData> dateEntry = new ArrayList<FileData>();
+                dateEntry.add(new FileData(data[0], data[1], data[2]));
+                dateTree.put(data[2],dateEntry);
             }
             sc.close();
         } catch (FileNotFoundException e) {
@@ -36,20 +40,49 @@ public class FileSystem {
     }
 
 
-    // TODO
     public void add(String name, String dir, String date) {
-    	
+        // NameMap add
+    	if ( !this.nameTree.containsKey(name) ) {
+            this.nameTree.put(name,new FileData(name, dir, date));
+        } else {
+            int comp = date.compareTo(this.nameTree.get(name).lastModifiedDate);
+            if ( comp < 0 && this.nameTree.get(name).dir.equals(dir) ) {
+                this.nameTree.replace(name,new FileData(name, dir, date));
+            }
+        }
+        // DateMap add
+        if ( !this.dateTree.containsKey(date) ) {
+            ArrayList<FileData> entry = new ArrayList<FileData>();
+            entry.add(new FileData(name,dir,date));
+            this.dateTree.put(date, entry);
+        } else {
+            ArrayList<FileData> existingEntry = this.dateTree.get(date);
+            for (FileData node : existingEntry ) {
+                if (!node.name.equals(name)) {
+                    existingEntry.add(new FileData(name, dir, date));
+                } 
+            }
+        }
+
     }
 
 
-    // TODO
     public ArrayList<String> findFileNamesByDate(String date) {
-        return null;
+        
+        ArrayList<String> names = new ArrayList<String>();
+        if ( this.dateTree.containsKey(date)) {
+            ArrayList<FileData> files = this.dateTree.get(date);
+            for ( int i = 0; i < files.size(); i++ ) {
+                names.add(files.get(i).name);
+            }
+        }
+        return names;
     }
 
 
     // TODO
     public FileSystem filter(String startDate, String endDate) {
+
         return null;
     }
     
@@ -60,9 +93,14 @@ public class FileSystem {
     }
     
     
-    // TODO
     public List<String> outputNameTree(){
-        return null;
+        ArrayList<String> str = new ArrayList<>();
+        List<String> names = this.nameTree.keys();
+        for ( int i = 0; i < names.size(); i++ ) {
+            String key = names.get(i);
+            str.add(key + this.nameTree.get(key).toString());
+        }
+        return str;
     }
     
     
